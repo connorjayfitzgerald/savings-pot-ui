d4d8c5ef5d591087300a8fe5af838d826d85fa95
Connor Fitzgerald <connorfitzgerald1995@aol.com>
2019-07-29 20:13:05 +0100

    Fixed sidedrawer scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ import { Summary } from './containers/Summary/Summary';
 // ----------------------------- FILE DEFINITION ------------------------------
 
 export const App: React.FC = (): JSX.Element => {
-    const [authenticated, setAuthenticated] = useState(true);
+    const [authenticated, setAuthenticated] = useState(false);
 
     const toggleAuth = () => setAuthenticated(prevState => !prevState);
 
